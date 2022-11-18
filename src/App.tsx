ecb48d32d9f27104c7c257fcb32039e97c7f0468
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-11-19 00:00:02 +0800

    setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1>Hello, React + Typescript + Tailwind CSS</h1>
+      <h1 className="text-xl underline text-blue-500">Hello, React + Typescript + Tailwind CSS</h1>
     </div>
   );
 };
