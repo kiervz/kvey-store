c6ba4334d8a12b4b1a2c02942b16edf283d5a2f6
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2022-12-28 14:18:15 +0800

    Updates

--- assets/Content.bf4b6ad6.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index.84fd7326.js";const e=({children:t})=>s("div",{className:"mt-14",children:t});export{e as Content,e as default};
--- assets/Home.fe254085.js
+++
@@ -1,1 +0,0 @@
-import{g as a,a as e,j as t}from"./index.84fd7326.js";const d=()=>{const r=a(s=>s.user.user);return e("div",{children:[t("p",{children:"Home"}),e("p",{children:["Name: ",r.data.name]}),e("p",{children:["Role: ",r.data.roles]})]})};export{d as default};
